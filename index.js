8f6d19447201e33d3c1177f7c787f70a23c01ef5
Cara Kane <cara.kane@gmail.com>
2017-11-08 17:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(block) {
-  return(block - 42)
+  block >= 42 ? block - 42 : 42 - block
 }
 
 function distanceFromHqInFeet() {
-  
+
 }
 
 function distanceTravelledInFeet() {
-  
+
 }
 
 function calculatesFarePrice(start, destination){
-  
+
 }
