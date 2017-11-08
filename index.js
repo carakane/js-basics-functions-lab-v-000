ddd212207b502bec81120b24af55219dd6f4ce74
Cara Kane <cara.kane@gmail.com>
2017-11-08 17:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(block) {
-  block >= 42 ? block - 42 : 42 - block
+  block >= 42 ? (block - 42) : (42 - block)
 }
 
 function distanceFromHqInFeet() {
